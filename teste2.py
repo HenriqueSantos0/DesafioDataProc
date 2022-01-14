a027c90503b905c1a242af2bfa879938b3926021
Lucas Henrique dos Santos Santana <89212899+HenriqueSantos0@users.noreply.github.com>
2022-01-13 21:24:43 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -28,3 +28,7 @@ df = df.merge(df_order_reviews, on='order_id', how='outer')
 df = df.merge(df_products, on='product_id', how='outer')
 df = df.merge(df_customer, on='customer_id', how='outer')
 df = df.merge(df_sellers, on='seller_id', how='outer')
+
+df=df[['customer_state', 'customer_city', 'customer_id', 'customer_unique_id', 'seller_state', 'seller_id', 'order_id', 'order_item_id', 'order_status', 'order_purchase_timestamp', 'order_approved_at', 'order_estimated_delivery_date', 'order_estimated_delivery_date', 'freight_value', 'price']]
+
+
